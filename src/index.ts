e87b681e8a7bcb8b8725919efa6720a0ac2bcab4
Shariful Islam <isharif766@gmail.com>
2025-05-08 00:23:16 +0600

    first problem updated

--- src/index.ts
+++ src/index.ts
@@ -14,7 +14,3 @@ function formatString (input:string,toUpper?:boolean):string {
     }
 }
 
-console.log(formatString('Hello'));
-console.log(formatString('Hello',true));
-console.log(formatString('Hello',false));
-
