8bd31b12045a5b081f8f8d5d21990be61b345242
Shariful Islam <isharif766@gmail.com>
2025-05-08 01:42:49 +0600

    fourth problem added

--- src/index.ts
+++ src/index.ts
@@ -24,4 +24,31 @@ function concatenateArrays<T>(...arrays: T[][]): T[] {
     return ([] as T[]).concat(...arrays)
 }
 
+// Problem-4
+class vehicle {
+    private make:string;
+    private year:number;
+
+    constructor(make:string,year:number){
+        this.make= make;
+        this.year= year;
+    }
+
+    public getInfo():string {
+       return  `Make: ${this.make},Year: ${this.year}`
+    }
+}
+
+class car extends vehicle {
+    private model: string;
+
+    constructor(make:string,year:number,model:string){
+        super(make,year)
+        this.model =model
+    }
+
+    public getModel():string{
+        return  `Model: ${this.model}`
+    }
+}
 
